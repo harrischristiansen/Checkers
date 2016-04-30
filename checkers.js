e107db4d5d2ad7c8d0f64f23605df2587deefeb2
Harris Christiansen (hc15ret2) <harrischristiansen@mac.com>
2016-04-29 21:59:39 -0400

    Fixed indentation

--- checkers.js
+++ checkers.js
@@ -8,11 +8,11 @@ Checkers - Javascript
 $(function() {
 	$(".redPiece").draggable( {
 		revert: "invalid",
-        create: function(){$(this).data('position',$(this).position())},
+		create: function(){$(this).data('position',$(this).position())},
 	});
 	$(".whitePiece").draggable( {
 		revert: "invalid",
-        create: function(){$(this).data('position',$(this).position())},
+		create: function(){$(this).data('position',$(this).position())},
 	});
 
 	$(".blackSquare").droppable({
@@ -24,7 +24,7 @@ $(function() {
 });
 
 function snapToMiddle(dragger, target){
-    var topMove = target.position().top - dragger.data('position').top + (target.outerHeight(true) - dragger.outerHeight(true)) / 2;
-    var leftMove= target.position().left - dragger.data('position').left + (target.outerWidth(true) - dragger.outerWidth(true)) / 2;
-    dragger.animate({top:topMove,left:leftMove},{duration:600,easing:'easeOutBack'});
+	var topMove = target.position().top - dragger.data('position').top + (target.outerHeight(true) - dragger.outerHeight(true)) / 2;
+	var leftMove= target.position().left - dragger.data('position').left + (target.outerWidth(true) - dragger.outerWidth(true)) / 2;
+	dragger.animate({top:topMove,left:leftMove},{duration:600,easing:'easeOutBack'});
 }
