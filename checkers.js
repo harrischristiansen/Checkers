38f6b92e26a88ffae286f424866648d986c9cd0e
Harris Christiansen (hc15ret2) <harrischristiansen@mac.com>
2016-04-29 21:58:43 -0400

    Centers pieces after move

--- checkers.js
+++ checkers.js
@@ -6,16 +6,25 @@ Checkers - Javascript
 */
 
 $(function() {
-	$(".redPiece").draggable({ revert: "invalid" });
-	$(".whitePiece").draggable({ revert: "invalid" });
+	$(".redPiece").draggable( {
+		revert: "invalid",
+        create: function(){$(this).data('position',$(this).position())},
+	});
+	$(".whitePiece").draggable( {
+		revert: "invalid",
+        create: function(){$(this).data('position',$(this).position())},
+	});
 
 	$(".blackSquare").droppable({
 		activeClass: "blackSquare",
 		drop: function( event, ui ) {
-			$(this)
-			.addClass( "ui-state-highlight" )
-			.find( "p" )
-			.html( "Dropped!" );
+			snapToMiddle(ui.draggable,$(this));
 		}
 	});
 });
+
+function snapToMiddle(dragger, target){
+    var topMove = target.position().top - dragger.data('position').top + (target.outerHeight(true) - dragger.outerHeight(true)) / 2;
+    var leftMove= target.position().left - dragger.data('position').left + (target.outerWidth(true) - dragger.outerWidth(true)) / 2;
+    dragger.animate({top:topMove,left:leftMove},{duration:600,easing:'easeOutBack'});
+}
