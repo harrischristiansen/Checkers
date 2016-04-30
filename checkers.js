e07f95006b95707556f1c9ae3ea394c0f0caf26a
Harris Christiansen (hc15ret2) <harrischristiansen@mac.com>
2016-04-29 22:27:33 -0400

    Fixed local file paths

--- checkers.js
+++ checkers.js
@@ -9,22 +9,28 @@ $(function() {
 	$(".redPiece").draggable( {
 		revert: "invalid",
 		create: function(){$(this).data('position',$(this).position())},
+		cursor: "move",
 	});
 	$(".whitePiece").draggable( {
 		revert: "invalid",
 		create: function(){$(this).data('position',$(this).position())},
+		cursor: "move",
 	});
 
 	$(".blackSquare").droppable({
 		activeClass: "blackSquare",
 		drop: function( event, ui ) {
-			snapToMiddle(ui.draggable,$(this));
+			movePiece(ui.draggable,$(this));
 		}
 	});
 });
 
+function movePiece(piece, sqaure) {
+	snapToMiddle(piece, square);
+}
+
 function snapToMiddle(dragger, target){
 	var topMove = target.position().top - dragger.data('position').top + (target.outerHeight(true) - dragger.outerHeight(true)) / 2;
 	var leftMove= target.position().left - dragger.data('position').left + (target.outerWidth(true) - dragger.outerWidth(true)) / 2;
-	dragger.animate({top:topMove,left:leftMove},{duration:600,easing:'easeOutBack'});
+	dragger.animate({top:topMove,left:leftMove},{duration:400,easing:'easeOutBack'});
 }
