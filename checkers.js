f983d1100d4c50c267e50dfcb5d6618fb838da64
Mehak <vohram@purdue.edu>
2016-05-02 09:57:24 -0400

    Added How to Play
    Rules have now been added. Better color system is also in place

--- checkers.js
+++ checkers.js
@@ -169,7 +169,7 @@ function updateTurnIfNecessary(pieceColor) {
 	if(pieceColor == "w") {
 		currentTurn = 1;
 		$("#currentTurn").text("Black");
-		$("#currentTurn").css("color", "#8B8989");
+		$("#currentTurn").css("color", "#8C8C8C");
 	} else if(pieceColor == "r") {
 		currentTurn = 0;
 		$("#currentTurn").text("White");
@@ -215,15 +215,3 @@ function snapToMiddle(dragger, target){
 	var leftMove= target.position().left - dragger.data('position').left + (target.outerWidth(true) - dragger.outerWidth(true)) / 2;
 	dragger.animate({top:topMove,left:leftMove},{duration:400,easing:'easeOutBack'});
 }
-
-	// // <!-- Google Analytics -->
-	// <script>
-	//   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	//   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	//   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	//   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-	//   ga('create', 'UA-77207822-1', 'auto');
-	//   ga('send', 'pageview');
-
-	// </script>
